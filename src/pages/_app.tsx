82e84f051581a363daf8808dda43f6966fd20c41
Henry Owens <henryowens@Henrys-MacBook-Pro.local>
2023-05-10 20:50:14 +0200

    Fix to html tag lang

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
         <div className="flex gap-2">
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
-        </div> 
+        </div>
       </div>
       <Component {...pageProps} />
     </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 import appConfig from "@/utils/appConfig";
 
 const MyDocument: React.FC = () => (
-  <Html lang={appConfig.title}>
+  <Html lang={appConfig.lang}>
     <Head />
     <body className="p-4">
       <Main />
